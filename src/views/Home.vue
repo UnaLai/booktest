991725db0ffb8b46a5d9b17b2bcd9066b37fb5e6
UnaLai <qqq323618@gmail.com>
2019-09-13 21:42:53 +0800

    [feat]:book test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="solid-container" style="height:100vh">
+      </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import testpdf from "@/assets/pdf/CondoLiving.pdf";
+// import flipbook from "@/config/3dflipbook.min.js";
+
+
 
 export default {
   name: "home",
   components: {
     HelloWorld
+  },
+  data(){
+    return{
+      testpdf:testpdf,
+    }
+  },
+  created(){
+    $('.solid-container').FlipBook({pdf: this.testpdf});
   }
 };
 </script>
